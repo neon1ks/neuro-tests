b6f3dcfd126691e54ba28d2ef3e8e634bb48b466
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2019-05-07 11:57:36 +0800

    Добавлены первые тесты

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
+#include <gtest/gtest.h>
+#include "narray.h"
 
-using namespace std;
+TEST(narray, test01) {
+	NArray<int> test_array;
+	ASSERT_EQ(int(0), test_array.getSize());
+	ASSERT_EQ(int(0), test_array.getLenght());
+	ASSERT_EQ(int(1), test_array.getBlock());
+	int *p = nullptr;
+	ASSERT_EQ(p, test_array.getData());
+}
 
-int main() {
-	cout << "Hello World!" << endl;
-	return 0;
+TEST(narray, test02) {
+	const int Len = 5;
+	NArray<int> test_array = NArray<int>(Len);
+	ASSERT_EQ(Len, test_array.getSize());
+
+	for (int i = 0; i < Len; ++i) {
+		test_array.set(i, i);
+		ASSERT_EQ(i, test_array.get(i));
+		ASSERT_EQ(i + 1, test_array.getLenght());
+	}
+}
+
+int main(int argc, char *argv[]) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
