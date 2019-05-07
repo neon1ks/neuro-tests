c3774fd0e94af49a74ca9933978e8b1140555c00
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2019-05-07 14:58:31 +0800

    Тест на работу метода NArray<NType>::resize()

--- main.cpp
+++ main.cpp
@@ -33,6 +33,38 @@ TEST(narray, test03) {
 	ASSERT_EQ(int(Len), test_array.getBlock());
 }
 
+// Проверка метода NArray<NType>::resize()
+TEST(narray, test04) {
+	NArray<int> test_array;
+	ASSERT_EQ(int(0), test_array.getSize());
+	ASSERT_EQ(int(0), test_array.getLenght());
+	int *p = nullptr;
+	ASSERT_EQ(p, test_array.getData());
+
+	// Увеличиваем m_size до 5
+	const int Five = 5;
+	ASSERT_NE(Five, test_array.getSize());
+	test_array.resize(Five);
+	ASSERT_EQ(Five, test_array.getSize());
+	ASSERT_EQ(int(0), test_array.getLenght());
+	ASSERT_NE(p, test_array.getData());
+
+	// Увеличиваем m_lenght до 3
+	const int Three = 3;
+	for (int i = 0; i < Three; ++i) {
+		test_array.set(i, i);
+		ASSERT_EQ(i, test_array.get(i));
+		ASSERT_EQ(i + 1, test_array.getLenght());
+	}
+
+	// Пробуем уменьшить m_size до 1
+	const int One = 1;
+	test_array.resize(One);
+	ASSERT_NE(One, test_array.getSize());
+	ASSERT_EQ(Three, test_array.getSize());
+	ASSERT_NE(Five, test_array.getSize());
+}
+
 int main(int argc, char *argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
