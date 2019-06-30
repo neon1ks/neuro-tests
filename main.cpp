fa387b81ac34f4ea5c34e60e8f186074ecc5a199
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2019-06-30 18:28:54 +0800

    Добавлены тесты на класс NMatrix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <string>
 #include "narray.h"
+#include "nmatrix.h"
+
+#define GTEST_COUT std::cerr
+
+void print_array(const NMatrix<int> &obj) {
+	int size_row = obj.getSizeRow();
+	int size_column = obj.getSizeColumn();
+	int len_row = obj.getLenRow();
+	int len_column = obj.getLenColumn();
+
+	int *data = obj.getData();
+
+	GTEST_COUT << "============================================================\n";
+	GTEST_COUT << "size_row = " << size_row;
+	GTEST_COUT << ", size_column = " << size_column;
+	GTEST_COUT << ", len_row = " << len_row;
+	GTEST_COUT << ", len_column = " << len_column << std::endl;
+
+	std::string out;
+
+	for (int i = 0; i < len_row; ++i) {
+		for (int j = 0; j < len_column; ++j) {
+			out += std::to_string(data[i * size_column + j]) + " ";
+		}
+		out += "\n";
+	}
+	GTEST_COUT << out;
+	GTEST_COUT << "============================================================\n";
+}
 
 TEST(narray, test01) {
 	NArray<int> test_array;
@@ -105,6 +136,366 @@ TEST(narray, test05) {
 	// Не совсем правильно, но пусть будет
 	for (int i = Three; i < test_array.getSize(); ++i) {
 		ASSERT_EQ(Seven, test_array.get(i));
+	}
+}
+
+// Проверка методов set(), get(), resize()
+TEST(nmatrix, test01) {
+	NMatrix<int> test_matrix;
+
+	const int size_row = 4;
+	const int size_column = 3;
+	const int new_size_row = 5;
+	const int new_size_column = 4;
+
+	test_matrix.init(size_row, size_column, 0);
+
+	for (int i = 0; i < size_row; ++i) {
+		for (int j = 0; j < size_column; ++j) {
+			test_matrix.set(i * j, i, j);
+		}
+	}
+
+	for (int i = 0; i < size_row; ++i) {
+		for (int j = 0; j < size_column; ++j) {
+			ASSERT_EQ(i * j, test_matrix.get(i, j));
+		}
+	}
+	test_matrix.resize(size_row - 1, size_column - 1);
+
+	ASSERT_EQ(size_row, test_matrix.getSizeRow());
+	ASSERT_EQ(size_column, test_matrix.getSizeColumn());
+	ASSERT_EQ(size_row, test_matrix.getLenRow());
+	ASSERT_EQ(size_column, test_matrix.getLenColumn());
+
+	test_matrix.resize(new_size_row, 0);
+
+	ASSERT_EQ(new_size_row, test_matrix.getSizeRow());
+	ASSERT_EQ(size_column, test_matrix.getSizeColumn());
+	ASSERT_EQ(size_row, test_matrix.getLenRow());
+	ASSERT_EQ(size_column, test_matrix.getLenColumn());
+
+	test_matrix.resize(0, new_size_column);
+
+	ASSERT_EQ(new_size_row, test_matrix.getSizeRow());
+	ASSERT_EQ(new_size_column, test_matrix.getSizeColumn());
+	ASSERT_EQ(size_row, test_matrix.getLenRow());
+	ASSERT_EQ(size_column, test_matrix.getLenColumn());
+
+	test_matrix.resize(new_size_row + 1, new_size_column + 1);
+
+	ASSERT_EQ(new_size_row + 1, test_matrix.getSizeRow());
+	ASSERT_EQ(new_size_column + 1, test_matrix.getSizeColumn());
+	ASSERT_EQ(size_row, test_matrix.getLenRow());
+	ASSERT_EQ(size_column, test_matrix.getLenColumn());
+
+	for (int i = 0; i < size_row; ++i) {
+		for (int j = 0; j < size_column; ++j) {
+			ASSERT_EQ(i * j, test_matrix.get(i, j));
+		}
+	}
+}
+
+// Проверка метода init()
+TEST(nmatrix, test02) {
+	NMatrix<int> test_matrix;
+
+	const int Zero = 0;
+	const int One = 1;
+	const int Two = 2;
+	const int Three = 3;
+	const int len_row = 4;
+	const int len_column = 3;
+
+	test_matrix.init(len_row, len_column, Zero);
+	for (int i = 0; i < len_row; ++i) {
+		for (int j = 0; j < len_column; ++j) {
+			ASSERT_EQ(Zero, test_matrix.get(i, j));
+		}
+	}
+	ASSERT_EQ(len_row, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+
+	test_matrix.init(len_row + 1, len_column - 1, One);
+	for (int i = 0; i < len_row + 1; ++i) {
+		for (int j = 0; j < len_column - 1; ++j) {
+			ASSERT_EQ(One, test_matrix.get(i, j));
+		}
+	}
+	ASSERT_EQ(len_row + 1, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 1, test_matrix.getLenRow());
+	ASSERT_EQ(len_column - 1, test_matrix.getLenColumn());
+
+	test_matrix.init(len_row - 1, len_column + 1, Two);
+	for (int i = 0; i < len_row - 1; ++i) {
+		for (int j = 0; j < len_column + 1; ++j) {
+			ASSERT_EQ(Two, test_matrix.get(i, j));
+		}
+	}
+	ASSERT_EQ(len_row + 1, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 1, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row - 1, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 1, test_matrix.getLenColumn());
+
+	test_matrix.init(len_row + 2, len_column + 2, Three);
+	for (int i = 0; i < len_row + 2; ++i) {
+		for (int j = 0; j < len_column + 2; ++j) {
+			ASSERT_EQ(Three, test_matrix.get(i, j));
+		}
+	}
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 2, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getLenColumn());
+}
+
+// Проверка методов addRow и delRow
+TEST(nmatrix, test03) {
+	NMatrix<int> test_matrix;
+
+	const int Zero = 0;
+	const int len_row = 4;
+	const int len_column = 3;
+
+	test_matrix.init(len_row + 2, len_column + 2, 0);
+	test_matrix.init(len_row, len_column, 0);
+	for (int i = 0; i < len_row; ++i) {
+		for (int j = 0; j < len_column; ++j) {
+			test_matrix.set(i, i, j);
+			ASSERT_EQ(i, test_matrix.get(i, j));
+		}
+	}
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+
+	test_matrix.addRow(2, 5);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 1, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+	for (int j = 0; j < len_column; ++j) {
+		ASSERT_EQ(Zero, test_matrix.get(0, j));
+		ASSERT_EQ(1, test_matrix.get(1, j));
+		ASSERT_EQ(5, test_matrix.get(2, j));
+		ASSERT_EQ(2, test_matrix.get(3, j));
+		ASSERT_EQ(3, test_matrix.get(4, j));
+	}
+
+	test_matrix.addRow(5, 6);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 2, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+	for (int j = 0; j < len_column; ++j) {
+		ASSERT_EQ(Zero, test_matrix.get(0, j));
+		ASSERT_EQ(1, test_matrix.get(1, j));
+		ASSERT_EQ(5, test_matrix.get(2, j));
+		ASSERT_EQ(2, test_matrix.get(3, j));
+		ASSERT_EQ(3, test_matrix.get(4, j));
+		ASSERT_EQ(6, test_matrix.get(5, j));
+	}
+
+	test_matrix.addRow(7, 7);
+	ASSERT_EQ(len_row + 4, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 4, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+	for (int j = 0; j < len_column; ++j) {
+		ASSERT_EQ(Zero, test_matrix.get(0, j));
+		ASSERT_EQ(1, test_matrix.get(1, j));
+		ASSERT_EQ(5, test_matrix.get(2, j));
+		ASSERT_EQ(2, test_matrix.get(3, j));
+		ASSERT_EQ(3, test_matrix.get(4, j));
+		ASSERT_EQ(6, test_matrix.get(5, j));
+		ASSERT_EQ(7, test_matrix.get(6, j));
+		ASSERT_EQ(7, test_matrix.get(7, j));
+	}
+
+	test_matrix.delRow(8);
+	ASSERT_EQ(len_row + 4, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 4, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+	for (int j = 0; j < len_column; ++j) {
+		ASSERT_EQ(Zero, test_matrix.get(0, j));
+		ASSERT_EQ(1, test_matrix.get(1, j));
+		ASSERT_EQ(5, test_matrix.get(2, j));
+		ASSERT_EQ(2, test_matrix.get(3, j));
+		ASSERT_EQ(3, test_matrix.get(4, j));
+		ASSERT_EQ(6, test_matrix.get(5, j));
+		ASSERT_EQ(7, test_matrix.get(6, j));
+		ASSERT_EQ(7, test_matrix.get(7, j));
+	}
+
+	test_matrix.delRow(7);
+	ASSERT_EQ(len_row + 4, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 3, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+	for (int j = 0; j < len_column; ++j) {
+		ASSERT_EQ(Zero, test_matrix.get(0, j));
+		ASSERT_EQ(1, test_matrix.get(1, j));
+		ASSERT_EQ(5, test_matrix.get(2, j));
+		ASSERT_EQ(2, test_matrix.get(3, j));
+		ASSERT_EQ(3, test_matrix.get(4, j));
+		ASSERT_EQ(6, test_matrix.get(5, j));
+		ASSERT_EQ(7, test_matrix.get(6, j));
+	}
+
+	test_matrix.delRow(4);
+	ASSERT_EQ(len_row + 4, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 2, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+
+	for (int j = 0; j < len_column; ++j) {
+		ASSERT_EQ(Zero, test_matrix.get(0, j));
+		ASSERT_EQ(1, test_matrix.get(1, j));
+		ASSERT_EQ(5, test_matrix.get(2, j));
+		ASSERT_EQ(2, test_matrix.get(3, j));
+		ASSERT_EQ(6, test_matrix.get(4, j));
+		ASSERT_EQ(7, test_matrix.get(5, j));
+	}
+
+	test_matrix.delRow(0);
+	ASSERT_EQ(len_row + 4, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row + 1, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+	for (int j = 0; j < len_column; ++j) {
+		ASSERT_EQ(1, test_matrix.get(0, j));
+		ASSERT_EQ(5, test_matrix.get(1, j));
+		ASSERT_EQ(2, test_matrix.get(2, j));
+		ASSERT_EQ(6, test_matrix.get(3, j));
+		ASSERT_EQ(7, test_matrix.get(4, j));
+	}
+}
+
+// Проверка методов addColumn и delColumn
+TEST(nmatrix, test04) {
+	NMatrix<int> test_matrix;
+
+	const int Zero = 0;
+	const int len_row = 3;
+	const int len_column = 4;
+
+	test_matrix.init(len_row + 2, len_column + 2, 0);
+	test_matrix.init(len_row, len_column, 0);
+	for (int i = 0; i < len_row; ++i) {
+		for (int j = 0; j < len_column; ++j) {
+			test_matrix.set(j, i, j);
+			ASSERT_EQ(j, test_matrix.get(i, j));
+		}
+	}
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column, test_matrix.getLenColumn());
+
+	test_matrix.addColumn(1, 5);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 1, test_matrix.getLenColumn());
+	for (int i = 0; i < len_row; ++i) {
+		ASSERT_EQ(Zero, test_matrix.get(i, 0));
+		ASSERT_EQ(5, test_matrix.get(i, 1));
+		ASSERT_EQ(1, test_matrix.get(i, 2));
+		ASSERT_EQ(2, test_matrix.get(i, 3));
+		ASSERT_EQ(3, test_matrix.get(i, 4));
+	}
+
+	test_matrix.addColumn(5, 6);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getLenColumn());
+	for (int i = 0; i < len_row; ++i) {
+		ASSERT_EQ(Zero, test_matrix.get(i, 0));
+		ASSERT_EQ(5, test_matrix.get(i, 1));
+		ASSERT_EQ(1, test_matrix.get(i, 2));
+		ASSERT_EQ(2, test_matrix.get(i, 3));
+		ASSERT_EQ(3, test_matrix.get(i, 4));
+		ASSERT_EQ(6, test_matrix.get(i, 5));
+	}
+
+	test_matrix.addColumn(7, 7);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 4, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 4, test_matrix.getLenColumn());
+	for (int i = 0; i < len_row; ++i) {
+		ASSERT_EQ(Zero, test_matrix.get(i, 0));
+		ASSERT_EQ(5, test_matrix.get(i, 1));
+		ASSERT_EQ(1, test_matrix.get(i, 2));
+		ASSERT_EQ(2, test_matrix.get(i, 3));
+		ASSERT_EQ(3, test_matrix.get(i, 4));
+		ASSERT_EQ(6, test_matrix.get(i, 5));
+		ASSERT_EQ(7, test_matrix.get(i, 6));
+		ASSERT_EQ(7, test_matrix.get(i, 7));
+	}
+
+	test_matrix.delColumn(8);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 4, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 4, test_matrix.getLenColumn());
+	for (int i = 0; i < len_row; ++i) {
+		ASSERT_EQ(Zero, test_matrix.get(i, 0));
+		ASSERT_EQ(5, test_matrix.get(i, 1));
+		ASSERT_EQ(1, test_matrix.get(i, 2));
+		ASSERT_EQ(2, test_matrix.get(i, 3));
+		ASSERT_EQ(3, test_matrix.get(i, 4));
+		ASSERT_EQ(6, test_matrix.get(i, 5));
+		ASSERT_EQ(7, test_matrix.get(i, 6));
+		ASSERT_EQ(7, test_matrix.get(i, 7));
+	}
+
+	test_matrix.delColumn(7);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 4, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 3, test_matrix.getLenColumn());
+	for (int i = 0; i < len_row; ++i) {
+		ASSERT_EQ(Zero, test_matrix.get(i, 0));
+		ASSERT_EQ(5, test_matrix.get(i, 1));
+		ASSERT_EQ(1, test_matrix.get(i, 2));
+		ASSERT_EQ(2, test_matrix.get(i, 3));
+		ASSERT_EQ(3, test_matrix.get(i, 4));
+		ASSERT_EQ(6, test_matrix.get(i, 5));
+		ASSERT_EQ(7, test_matrix.get(i, 6));
+	}
+
+	test_matrix.delColumn(3);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 4, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 2, test_matrix.getLenColumn());
+	for (int i = 0; i < len_row; ++i) {
+		ASSERT_EQ(Zero, test_matrix.get(i, 0));
+		ASSERT_EQ(5, test_matrix.get(i, 1));
+		ASSERT_EQ(1, test_matrix.get(i, 2));
+		ASSERT_EQ(3, test_matrix.get(i, 3));
+		ASSERT_EQ(6, test_matrix.get(i, 4));
+		ASSERT_EQ(7, test_matrix.get(i, 5));
+	}
+
+	test_matrix.delColumn(0);
+	ASSERT_EQ(len_row + 2, test_matrix.getSizeRow());
+	ASSERT_EQ(len_column + 4, test_matrix.getSizeColumn());
+	ASSERT_EQ(len_row, test_matrix.getLenRow());
+	ASSERT_EQ(len_column + 1, test_matrix.getLenColumn());
+	for (int i = 0; i < len_row; ++i) {
+		ASSERT_EQ(5, test_matrix.get(i, 0));
+		ASSERT_EQ(1, test_matrix.get(i, 1));
+		ASSERT_EQ(3, test_matrix.get(i, 2));
+		ASSERT_EQ(6, test_matrix.get(i, 3));
+		ASSERT_EQ(7, test_matrix.get(i, 4));
 	}
 }
 
